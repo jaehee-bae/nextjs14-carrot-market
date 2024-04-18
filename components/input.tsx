f2ac698b2231450525e55b7367dc565b61fa7112
jaehee-bae <hjaeb7777@gmail.com>
2024-04-18 23:53:48 +0900

    feat: #6.4 Refactor - HTMLInputElement

--- components/input.tsx
+++ components/input.tsx
@@ -1,4 +1,6 @@
-interface FormInputProps {
+import { InputHTMLAttributes } from "react";
+
+interface InputProps {
   type: string;
   placeholder: string;
   required: boolean;
@@ -6,21 +8,18 @@ interface FormInputProps {
   name: string;
 }
 
-export default function FormInput({
-  type,
-  placeholder,
-  required,
-  errors = [],
+export default function Input({
   name,
-}: FormInputProps) {
+  errors = [],
+  ...rest
+}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
+  console.log(rest);
   return (
     <div className="flex flex-col gap-2">
       <input
         name={name}
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
-        type={type}
-        placeholder={placeholder}
-        required={required}
+        {...rest}
       />
       {errors.map((error, index) => (
         <span key={index} className="text-red-500 font-medium">
