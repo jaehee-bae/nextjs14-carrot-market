2a055b26b274ba65390e0a3b8365bb88e680dda8
jaehee-bae <hjaeb7777@gmail.com>
2024-05-01 13:54:52 +0900

    feat: #8.1 Database Validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,6 +1,29 @@
 "use server";
-import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "../../lib/constants";
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR } from "@/lib/constants";
+import db from "@/lib/db"
 import { z } from "zod";
+
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+}
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: { email, },
+    select: {
+      id: true,
+    },
+  });
+  return Boolean(user) === false;
+}
 
 const formSchema = z.object({
   username: z
@@ -10,15 +33,20 @@ const formSchema = z.object({
     })
     .trim()
     .toLowerCase()
-    .transform((username) => `${username}⭐️`),
-  email: z.string().email().toLowerCase(),
+    .refine(checkUniqueUsername, "This username is already taken"),
+    // .transform((username) => `${username}⭐️`),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkUniqueEmail, "This email already taken"),
   password: z
     .string()
-    .min(PASSWORD_MIN_LENGTH)
-    .regex(
-      PASSWORD_REGEX,
-      PASSWORD_REGEX_ERROR,
-    ),
+    .min(PASSWORD_MIN_LENGTH),
+    // .regex(
+    //   PASSWORD_REGEX,
+    //   PASSWORD_REGEX_ERROR,
+    // ),
   confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
 })
   .superRefine(({ password, confirm_password }, ctx) => {
@@ -38,10 +66,13 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get("password"),
     confirm_password: formData.get("confirm_password"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    // hash password
+    // save the user to db
+    // log the user in
+    // redirect "/home"
   }
 }
