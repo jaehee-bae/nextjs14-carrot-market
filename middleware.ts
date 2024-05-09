71ed3c3191592b11324beaa926e83c3db5f1d31e
jaehee-bae <hjaeb7777@gmail.com>
2024-05-09 19:37:34 +0900

    feat: #8.12 Authentication Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
+interface Routes {
+  [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+  "/": true,
+  "/login": true,
+  "/sms": true,
+  "/create-account": true,
+};
+
 export async function middleware(request: NextRequest) {
   console.log(request.nextUrl);
+  const session = await getSession();
+  const exists = publicOnlyUrls[request.nextUrl.pathname];
+  if (!session.id) {
+    // not log in
+    if (!exists) {
+      // not allowed page
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  } else {
+    // log in, can't go publicUrls
+    if (exists) {
+      return NextResponse.redirect(new URL("/products", request.url));
+    }
+  }
 }
 
 export const config = {
