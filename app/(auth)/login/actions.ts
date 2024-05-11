0c2e7125e035dbfbcb91e11b5b5369ab04cdeb7d
jaehee-bae <hjaeb7777@gmail.com>
2024-05-11 12:02:32 +0900

    feat: #10.1 Tab Bar

--- app/(auth)/login/actions.ts
+++ app/(auth)/login/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { z } from "zod";
-import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR, } from "../../lib/constants";
+import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX, PASSWORD_REGEX_ERROR, } from "../../../lib/constants";
 import db from "@/lib/db";
 import bcrypt from "bcrypt"
 import getSession from "@/lib/session";
