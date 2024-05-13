3dbd9db16b5df571763763fb16882508b38f2c69
jaehee-bae <hjaeb7777@gmail.com>
2024-05-13 19:13:10 +0900

    feat: #10.3 Product Component

---
+++ components/list-product.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ListProductProps {
+  id: number,
+  photo: string,
+  title: string,
+  created_at: Date,
+  price: number,
+}
+
+export default function ListProduct({
+  id,
+  photo,
+  title,
+  created_at,
+  price
+}: ListProductProps) {
+  return (
+    <Link href={`/products/${id}`} className="flex gap-5">
+      <div className="relative size-28 rounded-md overflow-hidden">
+        <Image fill src={photo} alt={title} />
+      </div>
+      <div className="flex flex-col gap-1 *:text-white">
+        <span className="text-lg">{title}</span>
+        <span className="text-sm text-neutral-500">
+          {created_at.toString()}
+        </span>
+        <span className="text-lg font-semibold">{price}</span>
+      </div>
+    </Link>
+  );
+}
